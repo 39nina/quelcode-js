1b74e12758c279c7cab4ed06ba64231f33ea6176
39nina <392mknn@gmail.com>
2020-10-08 14:51:39 +0900

    Fix：命名変更、JSONstringifyメソッドの削除、テンプレートリテラルの使用

--- js-weather/weather.js
+++ js-weather/weather.js
@@ -1,27 +1,27 @@
 const request = new XMLHttpRequest();
-let data = "";
 
 const getdata = function() {
     // プルダウンで選択されている都市名を取得
     const city = document.getElementById("select").value;
 
     // 選択された都市データを参照
-    request.open ('GET', 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=4b5774e9f3d2a07b84f0f2f88e486224', true);
+    request.open ('GET', `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4b5774e9f3d2a07b84f0f2f88e486224`, true);
     request.responseType = 'json';
 
     request.send();
 
-    request.onload = function() {
-        data = this.response;
+    request.onload = () => {
+        const data = request.response;
 
-        const dataStringify1 = (JSON.stringify(data["name"])).split('\"').join('');
-        const dataStringify2 = (JSON.stringify(data["weather"][0]["main"])).split('\"').join('');
-        const dataStringify3 = JSON.stringify(data["main"]["temp_max"]);
-        const dataStringify4 = JSON.stringify(data["main"]["temp_min"]);
-        document.getElementById("output1").innerHTML = dataStringify1;
-        document.getElementById("output2").innerHTML = dataStringify2 ;
-        document.getElementById("output3").innerHTML = ((Math.round((dataStringify3 - 273.15)*10)/10).toFixed(1)) + "℃";
-        document.getElementById("output4").innerHTML = ((Math.round((dataStringify4 - 273.15)*10)/10).toFixed(1)) + "℃";
+        const name = data["name"];
+        const weather = data["weather"][0]["main"];
+        const weather2 = data["weather"][0]["icon"];
+        const hightemp = data["main"]["temp_max"];
+        const lowtemp = data["main"]["temp_min"];
+        document.getElementById("city").innerHTML = name;
+        document.getElementById("weather").innerHTML = weather;
+        document.getElementById("hightemp").innerHTML = ((Math.round((hightemp - 273.15)*10)/10).toFixed(1)) + "℃";
+        document.getElementById("lowtemp").innerHTML = ((Math.round((lowtemp - 273.15)*10)/10).toFixed(1)) + "℃";
     };
 }
 
@@ -32,3 +32,4 @@ getdata();
 document.getElementById("select").onchange = function() {
     getdata();
 };
+
